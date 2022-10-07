c0ce6ef5bbb4d836f8bb21011ba9559330a1d355
USER <USER@DESKTOP-TRQG01D>
2022-10-07 18:41:41 +0300

    Добавьте файлы проекта.

---
+++ Pr6.Dop.10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = 6, result; // присвоение локальной переменной n целого типа "byte"
+
+            // Умножить на 2
+            result = (byte)(n << 1); // 
+            Console.WriteLine("{0} * 2 = {1}", n, result); //вывод строки "..." с указанными данными на экран.
+
+            // Умножить на 4
+            result = (byte)(n << 2);
+            Console.WriteLine("{0} * 4 = {1}", n, result); //вывод строки "..." с указанными данными на экран.
+
+            // Разделить на 2
+            result = (byte)(n >> 1);
+            Console.WriteLine("{0} / 2 = {1}", n, result); //вывод строки "..." с указанными данными на экран.
+
+            Console.ReadLine(); //чтение данных.
+        }
+    }
+}
